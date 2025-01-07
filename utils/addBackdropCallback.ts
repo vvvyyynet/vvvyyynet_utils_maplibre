d241d860b0c2d09b99cd527484db02dbe48a768e
vvvyyynet <63237439+vvvyyynet@users.noreply.github.com>
2025-01-07 10:42:44 +0100

    chore: rename property

--- utils/addBackdropCallback.ts
+++ utils/addBackdropCallback.ts
@@ -1,6 +1,6 @@
 import { setNested } from './general_utils';
 
-function tweakBackdropStyle(styleset, type, backdropPath) {
+function tweakBackdropStyle(styleset, type, pathSuffix) {
 	// All replacements will happen on a styleset level.
 	// Input: type = 'lines' or 'polygons'
 
@@ -10,10 +10,10 @@ function tweakBackdropStyle(styleset, type, backdropPath) {
 	// If backdrop.minzoom (backdrop.maxzoom) is unset, make sure it has the same value as the regular line.
 	if (styleset?.[type] && !styleset?.[type]?.backdrop?.minzoom) {
 		//!! (testing for styleset?.[type]) is important, if styleset={})
-		setNested(styleset?.[type], `${backdropPath}.minzoom`, styleset?.[type]?.minzoom);
+		setNested(styleset?.[type], `${pathSuffix}.minzoom`, styleset?.[type]?.minzoom);
 	}
 	if (styleset?.[type] && !styleset?.[type]?.backdrop?.maxzoom) {
-		setNested(styleset?.[type], `${backdropPath}.maxzoom`, styleset?.[type]?.maxzoom);
+		setNested(styleset?.[type], `${pathSuffix}.maxzoom`, styleset?.[type]?.maxzoom);
 	}
 	return styleset;
 }
@@ -46,19 +46,19 @@ export const addBackdropCallback = (
 	if (layerType === 'symbol') {
 		if (c(stylesetPath, 'addSymbolBackdrop', null, { forceSkipValidation: true })) {
 
-			// Define BackdropPath used in stylesets
-			const backdropPath = 'symbolBackdrop';
+			// Define pathSuffix used in stylesets
+			const pathSuffix = 'symbolBackdrop';
 
 			// Tweak stylesets
-			collStyleset = tweakBackdropStyle(collStyleset, stylesetPath, backdropPath);
-			collStyleset.force = tweakBackdropStyle(collStyleset.force, stylesetPath, backdropPath);
-			featStyleset = tweakBackdropStyle(featStyleset, stylesetPath, backdropPath);
-			presetStyleset = tweakBackdropStyle(presetStyleset, stylesetPath, backdropPath);
+			collStyleset = tweakBackdropStyle(collStyleset, stylesetPath, pathSuffix);
+			collStyleset.force = tweakBackdropStyle(collStyleset.force, stylesetPath, pathSuffix);
+			featStyleset = tweakBackdropStyle(featStyleset, stylesetPath, pathSuffix);
+			presetStyleset = tweakBackdropStyle(presetStyleset, stylesetPath, pathSuffix);
 
 			// Adapt layerId and path 
 			//!TODO softcode this
 			layerId = [prefix, layerId, suffix].filter(Boolean).join(seperator);
-			stylesetPath = [stylesetPath, backdropPath].join('.');
+			stylesetPath = [stylesetPath, pathSuffix].join('.');
 
 			// Add a 'circle'-Layer (even though listening to 'symbol' above!)
 			map = addLayer(map, layerId, sourceId, groupNames, filterId, 'circle', c, stylesetPath);
